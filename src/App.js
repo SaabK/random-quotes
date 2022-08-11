add2f94cba3150d6bf85ce0c82478b004deead31
Ali Bin Naseer <aliisinsane0089@gmail.com>
2022-08-11 13:04:02 +0300

    Completed V1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const quotesURL =
+		'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+
+	const [quotes, setQuotes] = useState([]);
+	const [randomQuote, setRandomQuote] = useState('');
+
+	const fetchQuotes = useCallback(() => {
+		fetch(quotesURL)
+			.then(response => response.json())
+			.then(data => {
+				setQuotes(data.quotes);
+				const randomIndex = Math.floor(Math.random() * data.quotes.length);
+				setRandomQuote(data.quotes[randomIndex]);
+			})
+			.catch(err => console.error(err));
+
+		// console.log(quotes);
+	}, [quotesURL]);
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const handleNewQuote = () => {
+		const randomIndex = Math.floor(Math.random() * quotes.length);
+		setRandomQuote(quotes[randomIndex]);
+	};
+
+	const twitterIntentQuote = randomQuote.quote?.split(' ').join('%20');
+	const twitterIntentAuthor = randomQuote.author?.split(' ').join('%20');
+
+	useEffect(() => {
+		fetchQuotes();
+	}, []);
+
+	return (
+		<div id='quote-bg'>
+			<div id='quote-box'>
+				<div id='up'>
+					<div id='text'>{randomQuote.quote}</div>
+				</div>
+				<div id='author'>- {randomQuote.author}</div>
+				<div id='down'>
+					<button id='new-quote' className='rounded' onClick={handleNewQuote}>
+						New Quote
+					</button>
+					<a
+						href={`http://twitter.com/intent/tweet?text="${twitterIntentQuote}"%20-%20${twitterIntentAuthor}%20@AliBinTweets`}
+						id='tweet-quote'
+						className='rounded'>
+						<FontAwesomeIcon icon={faTwitter} color='#fff' />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
